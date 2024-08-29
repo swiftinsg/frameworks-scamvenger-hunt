35da05e25096f21d83a300069182949edd3a76f9
Jia Chen <github@jiachen.app>
2024-08-29 17:07:46 +0800

    Initial Commit

---
+++ Scamvenger Hunt/Scamvenger_HuntApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scamvenger_HuntApp.swift
+//  Scamvenger Hunt
+//
+//  Created by Jia Chen Yee on 8/29/24.
+//
+
+import SwiftUI
+
+@main
+struct Scamvenger_HuntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
