67178f4422d3259377689ce8c156f18debccde7c
Tristan Chay <tristanchay123@gmail.com>
2024-09-06 17:35:16 +0800

    feat: finished first page of meeting

--- Scamvenger Accessibility/Views/ContentView.swift
+++ Scamvenger Accessibility/Views/ContentView.swift
@@ -10,60 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         TabView {
-            ScamvengerPlanMinutes()
+            Meeting1()
             Text("This is Page 2")
         }
-        .tabViewStyle(.page)
+        .tabViewStyle(.page(indexDisplayMode: .always))
     }
-}
-
-struct StyledPage<Content: View>: View {
-    
-    var title: String
-    var subtitle: String = ""
-    
-    @ViewBuilder var content: () -> Content
-    
-    var body: some View {
-        VStack {
-            HStack {
-                VStack(alignment: .leading) {
-                    Text(title)
-                        .font(.title)
-                        .fontWeight(.bold)
-                        .foregroundStyle(.tint)
-                    if !subtitle.isEmpty {
-                        Text(subtitle)
-                            .font(.title3)
-                            .fontWeight(.medium)
-                    }
-                }
-                
-                
-                Spacer()
-                
-                Text(Date.now.addingTimeInterval(-86400), style: .date)
-                    .foregroundStyle(.secondary)
-            }
-            .padding()
-            
-            Divider()
-            
-            content()
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-        }
-        .padding()
-    }
-}
-
-struct ScamvengerPlanMinutes: View {
-    var body: some View {
-        StyledPage(title: "Scamvenger Meeting - Minutes", subtitle: "In Attendance: Sean, Shaun, Shawn, and Shon") {
-            Text("hello")
-        }
-    }
-}
-
-#Preview {
-    ContentView()
 }
