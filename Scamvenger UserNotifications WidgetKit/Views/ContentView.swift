fbbab0ee72b2fcaeaff06507de68ea1b426ef010
Tristan Chay <tristanchay123@gmail.com>
2024-09-06 13:40:36 +0800

    feat: removed pw lock for notifications activity app

--- Scamvenger UserNotifications WidgetKit/Views/ContentView.swift
+++ Scamvenger UserNotifications WidgetKit/Views/ContentView.swift
@@ -10,61 +10,25 @@ import WidgetKit
 
 struct ContentView: View {
     
-    @State private var text = ""
-    @State private var showingControls = false
     @State private var notifications = Notifications()
-    
-    @Environment(\.scenePhase) var scenePhase
     
     var body: some View {
         VStack {
-            if !showingControls {
-                Text("Station master's controls. If you're a student, this is not part of the activity. Exit the app.")
-                HStack {
-                    SecureField("Enter password", text: $text)
-                        .frame(width: 200)
-                        .textFieldStyle(.roundedBorder)
-                    Button("Enter") {
-                       verifyPassword()
+            HStack {
+                Button("Start/Reset Activity") {
+                    Task {
+                        await resetWidgets()
                     }
-                    .onSubmit {
-                        verifyPassword()
-                    }
+                    notifications.sendNewBatch()
                 }
-            } else {
-                Text("Start/Reset the activity before handing the iPad over to the new group.")
-                HStack {
-                    Button("Start/Reset Activity") {
-                        Task {
-                            await resetWidgets()
-                        }
-                        notifications.sendNewBatch()
-                    }
-                    .buttonStyle(.borderedProminent)
-                    Button("Stop Notifications", role: .destructive) {
-                        notifications.stop()
-                    }
-                    .buttonStyle(.borderedProminent)
+                .buttonStyle(.borderedProminent)
+                Button("Stop Notifications", role: .destructive) {
+                    notifications.stop()
                 }
+                .buttonStyle(.borderedProminent)
             }
         }
         .padding()
-        .onChange(of: scenePhase) {
-            if scenePhase != .active {
-                withAnimation {
-                    showingControls = false
-                }
-            }
-        }
-    }
-    
-    func verifyPassword() {
-        if text == "sigma" {
-            withAnimation {
-                showingControls = true
-                text = ""
-            }
-        }
     }
     
     func resetWidgets() async {
